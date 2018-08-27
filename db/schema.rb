019b975b29ad1da50a7b3941290cacf3a0d99515
github username <github email address>
2018-08-27 15:31:45 -0400

    db changes

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_06_18_215117) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "habits", force: :cascade do |t|
     t.string "name"
     t.text "description"
