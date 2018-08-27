04b4b264c54ea1e1f6db5e9f0f0e70c8ffbca32a
github username <github email address>
2018-08-27 15:45:18 -0400

    removes byebug from user controller for heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,11 +12,8 @@ class UsersController < ApplicationController
   def create
      @user = User.new
      if @user.valid?
-       byebug
-    # #we can validate for uniqueness and not have to do a find or create by imo? but that's a design choice & i'm flexible
       @user.save
       session[:user_id] = @user.id
-    # #redirect to @user show page
       redirect_to user_path(@user)
      else
     # #show error
